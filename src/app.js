c9de2d86783ead80feb4dcebd0e12bb1cdf14fb8
mauriciogaray10 <garaycarlosmauricio17@gmail.com>
2023-05-31 18:44:31 -0300

    utils.js done

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
 import express from 'express';
-import { Router } from 'express';
+import __dirname from './utils.js';
+import routerProducts from './routes/products.router.js';
+import routerCarts from './routes/carts.router.js';
 
 const app = express();
-app.listen(8080, ()=>{console.log();})
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+app.use('/api/products/', routerProducts);
+app.use('/api/carts/', routerCarts);
+
+
+
+
+
+
+
+
+app.listen(8080, ()=>{console.log('server is working')})
