d2e24a418d4fcbc16dadf88fafad56a299f67304
mauriciogaray10 <garaycarlosmauricio17@gmail.com>
2023-05-31 21:58:10 -0300

    products.router.js done

--- src/app.js
+++ src/app.js
@@ -9,13 +9,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
+
 app.use('/api/products/', routerProducts);
 app.use('/api/carts/', routerCarts);
-
-
-
-
-
 
 
 
---
+++ src/routes/products.router.js
@@ -0,0 +1,40 @@
+import { Router} from "express";
+import productManager from '../classes/ProductManager.js'
+
+const router = Router();
+
+const list = new productManager();
+
+router.get('/', async (res, req) => {
+    let lista = await list.getProducts();
+    return res.send(lista);
+})
+
+router.get('/:id', async ( res, req)=> {
+    let id = req.params.id;
+    const productById = await list.getProductById(id);
+    return res.send(productById); 
+})
+
+router.post('/', async (res, req) => {
+    let product = req.body;
+    if(!product){
+        return res.send('It is a empty product');
+    }else{
+        await list.addProduct(product);
+        return res.send('product added');
+    }
+})
+
+router.put('/', (res, req)=>{
+    let product = req.body;
+    list.updateProduct(product);
+    return res.send('Product modified');
+    
+})
+router.get('/:id', async ( res, req)=> {
+    let id = req.params.id;
+    const productById = await list.deleteProductbyId(id);
+    return res.send('Product deleted'); 
+})
+
